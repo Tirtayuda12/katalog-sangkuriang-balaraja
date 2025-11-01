3dd00f80ab63daf599bc3a62ad12a02b2be79e78
Tirtayuda12 <tyuda3851@gmail.com>
2025-11-01 11:41:09 +0700

    update halaman utama

--- src/app/page.js
+++ src/app/page.js
@@ -22,7 +22,7 @@ export default function HomePage() {
   return (
     <main className="p-6 bg-green-50 min-h-screen">
       <h1 className="text-3xl font-bold mb-6 text-green-700 text-center">
-        Katalog Produk Pertanian & Peternakan
+        Sangkuriang Katalog
       </h1>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 max-w-6xl mx-auto">
